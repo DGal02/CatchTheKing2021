a7865aabe4c4c13d17ac942757ed68840340d385
DGal02 <dominik.galczyk@gmail.com>
2021-01-05 02:55:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+
+    $scope.number = 25;
+    $scope.cards = ["1","2","3","4","5","uwaga","krol","pole"];
+    $scope.changeCard = function(id,card){
+        let setCard = $("#" + id).attr("set-card");
+        if ($("#" + id).attr("fade") == 0) {
+            if (card !== setCard){
+                $("#" + id).removeClass("fade0");
+                $("#" + id).addClass("fade1");
+                $("#" + id).attr("set-card", card);
+                setTimeout(function () {
+                    $("#" + id).attr("src", 'images/' + card + ".jpg");
+                    $("#" + id).attr("fade", 1);
+                    
+                }, 125);
+            }
+
+        } else {
+            if (card !== setCard){
+                $("#" + id).removeClass("fade1");
+                $("#" + id).addClass("fade0");
+                $("#" + id).attr("set-card", card);
+                setTimeout(function () {
+                    $("#" + id).attr("src", 'images/' + card + ".jpg");
+                    $("#" + id).attr("fade", 0);
+                    
+                }, 125);
+            }
+           
+            
+        }
+    };
+    
+});
+
+
+
+
