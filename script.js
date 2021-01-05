8aaf22ba80cf576d229c64720bdddf880248d998
DGal02 <dominik.galczyk@gmail.com>
2021-01-05 03:12:29 +0100

    recomposition

--- script.js
+++ script.js
@@ -2,36 +2,34 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
 
     $scope.number = 25;
-    $scope.cards = ["1","2","3","4","5","uwaga","krol","pole"];
-    $scope.changeCard = function(id,card){
+    $scope.cards = ["1", "2", "3", "4", "5", "uwaga", "krol", "pole"];
+    $scope.changeCard = function (id, card) {
         let setCard = $("#" + id).attr("set-card");
-        if ($("#" + id).attr("fade") == 0) {
-            if (card !== setCard){
+        if (card !== setCard) {
+            if ($("#" + id).attr("fade") === "0") {
                 $("#" + id).removeClass("fade0");
                 $("#" + id).addClass("fade1");
                 $("#" + id).attr("set-card", card);
                 setTimeout(function () {
                     $("#" + id).attr("src", 'images/' + card + ".jpg");
-                    $("#" + id).attr("fade", 1);
-                    
+                    $("#" + id).attr("fade", "1");
+
                 }, 125);
             }
-
-        } else {
-            if (card !== setCard){
+            else {
                 $("#" + id).removeClass("fade1");
                 $("#" + id).addClass("fade0");
                 $("#" + id).attr("set-card", card);
                 setTimeout(function () {
                     $("#" + id).attr("src", 'images/' + card + ".jpg");
-                    $("#" + id).attr("fade", 0);
-                    
+                    $("#" + id).attr("fade", "0");
+
                 }, 125);
             }
-           
-            
         }
-    };
+
+ 
+};
     
 });
 
