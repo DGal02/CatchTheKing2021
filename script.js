542a2679aa9b03af0c1ad7e1e081d884326704df
DGal02 <dominik.galczyk@gmail.com>
2021-01-07 00:41:11 +0100

    poprawa kodu

--- script.js
+++ script.js
@@ -28,41 +28,9 @@ app.controller('myCtrl', function ($scope) {
             }
         }
     }
-    $scope.restart = function () {
-        let resetCard = "pole";
-        $(".pole").each(function(){
-            let card = $(this).attr("set-card");
-            let cardID = $(this).attr("id");
-            if ( card !== resetCard) {
-                if ($(this).attr("fade")==="0" ) {
 
-                    $(this).removeClass("fade0");
-                    $(this).addClass("fade1");
-                    $(this).attr("set-card", resetCard);
-                    setTimeout(function () {
-                        $("#" + cardID).attr("src", 'images/' + resetCard + ".jpg");
-                        $("#" + cardID).attr("fade", "1");
-    
-                    }, 125);
-                }
-                else {
-                    
-                    $(this).removeClass("fade1");
-                    $(this).addClass("fade0");
-                    $(this).attr("set-card", resetCard);
-                    setTimeout(function () {
-                        $("#" + cardID).attr("src", 'images/' + resetCard + ".jpg");
-                        $("#" + cardID).attr("fade", "0");
-                        
-    
-                    }, 125);
-                }
-            }
-        });
-    }
-
-    $scope.taktyka = function () {
-        let resetCard = "uwaga";
+    $scope.changeAll = function (card) {
+        let resetCard = card;
         $(".pole").each(function(){
             let card = $(this).attr("set-card");
             let cardID = $(this).attr("id");
